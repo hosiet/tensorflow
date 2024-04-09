6bb928f9d4c7de2d843b7b587174fcf777485ca7
Boyuan Yang <073plan@gmail.com>
2024-04-09 15:02:28 -0400

    [RESEARCH] gpu_compatibility.cc: Allow Int8/UInt8 inputs and outputs

--- tensorflow/lite/tools/versioning/gpu_compatibility.cc
+++ tensorflow/lite/tools/versioning/gpu_compatibility.cc
@@ -481,10 +481,17 @@ absl::Status CheckGpuDelegateCompatibility(const OpSignature& op_sig) {
                   op_sig.inputs.at(0).type == kTfLiteFloat32) &&
                  op_sig.outputs.at(0).type == kTfLiteBool) {
         return absl::OkStatus();
+      // RESEARCH_MODIFICATION: START: allow int8/uint8 input and output
       } else if ((op_sig.inputs.at(0).type == kTfLiteFloat32 ||
-                  op_sig.inputs.at(0).type == kTfLiteInt32) &&
+                  op_sig.inputs.at(0).type == kTfLiteInt32 ||
+                  op_sig.inputs.at(0).type == kTfLiteUInt8 ||
+                  op_sig.inputs.at(0).type == kTfLiteInt8) &&
                  (op_sig.outputs.at(0).type == kTfLiteFloat32 ||
-                  op_sig.outputs.at(0).type == kTfLiteInt32)) {
+                  op_sig.outputs.at(0).type == kTfLiteInt32 ||
+                  op_sig.outputs.at(0).type == kTfLiteUInt8 ||
+                  op_sig.outputs.at(0).type == kTfLiteInt8
+                  )) {
+      // RESEARCH_MODIFICATION: END
         return absl::OkStatus();
       } else {
         return absl::UnimplementedError(absl::StrCat(
